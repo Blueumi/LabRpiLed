05392a1fb2095d8ac95a3b07a8047d3edd49a288
sprach <sprach21@daum.net>
2019-10-16 05:50:51 +0900

    create & update docs

---
+++ docs/faq.md
@@ -0,0 +1,5 @@
+﻿# FAQ
+
+> Visual Studio Code를 열면 원격접속(Remote-SSH)을 시도하면서 연결 오류가 발생합니다. 접속을 종료하고 싶은데 어떻게 하나요?
+  1. 'Could not establish connection to "&lt;<i>IP Addr</i>&gt;".'가 나오면 'More Actions...' 버튼을 클릭합니다.
+  2. 메뉴의 '파일' &gt; '원격 연결 종료'를 선택합니다.
--- src/pysrc/rpirgbled-01.py
+++ src/pysrc/rpirgbled-01.py
@@ -1,86 +1,77 @@
-# rpirgbled-01.py
+﻿# rpirgbled-01.py
 # 1초마다 R, G, B 를 순차적으로 On 시킨다.
 import RPi.GPIO as GPIO
 import time
 
 # RGB 핀 번호
-#PIN_R = 10  # pin seq:10, no:19
-#PIN_G = 9   # pin seq:11, no:21
-#PIN_B = 11  # pin seq:12, no:23
-LED_PINS = [ 10, 9, 11 ]    # RGB 핀 번호
-PIN_R = LED_PINS[0]         # RED
-PIN_G = LED_PINS[1]         # GREEN
-PIN_B = LED_PINS[2]         # BLUE
+PIN_R = 10  # pin seq:10, no:19
+PIN_G = 9   # pin seq:11, no:21
+PIN_B = 11  # pin seq:12, no:23
 
 # GPIO 초기화
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 # LED 공통 핀이 GND인 경우에는 ON이 HIGH, OFF가 LOW
-# LED 공통 핀이 +인 경우에는 ON이 HIGH, OFF가 LOW
-# 풀업(Pull-up)인 경우에는 반대
+# LED 공통 핀이 +인 경우에는 ON이 LOW, OFF가 HIGH
 
-# 현재 테스트용 RGB LED는 공통핀이 +라서 LED가 ON되려면 할당 핀에 LOW 신호를 주어야 한다.
+# LED는 공통핀이 + 인 경우
 LED_ON = GPIO.LOW
 LED_OFF = GPIO.HIGH
 
-# LED 핀 속성 정의 및 초기화
-for i in range(0, 3):
-    GPIO.setup(LED_PINS[i], GPIO.OUT)
-    GPIO.output(LED_PINS[i], LED_OFF)
+# LED는 공통핀이 GND 인 경우
+#LED_ON = GPIO.HIGH
+#LED_OFF = GPIO.LOW
 
-# leds on
-print('> Red, Green, Blue led를 1초마다 On 시킨다.')
+# LED 핀 속성 정의
+GPIO.setup(PIN_R, GPIO.OUT)
+GPIO.setup(PIN_G, GPIO.OUT)
+GPIO.setup(PIN_B, GPIO.OUT)
+
+# LED 핀 초기 신호 설정
+GPIO.output(PIN_R, LED_OFF)
+GPIO.output(PIN_G, LED_OFF)
+GPIO.output(PIN_B, LED_OFF)
+
+# 타이틀 메시지 출력
+print('> Red, Green, Blue led를 1초마다 번갈아 가면서 On 시킨다.')
 
 try:
-    pos_on_led = -1;        # 처음에는 On된 LED가 없다.
+    led_index = -1;        # 처음에는 On된 LED가 없다.
 
     while True:
         # LED 위치를 지정한다.
-        pos_on_led += 1     # python에서는 ++ 미지원
-        if pos_on_led > 2:
-            pos_on_led = 0
+        # led_index: 0-Red, 1-Green, 2-Blue
+        led_index += 1     # python에서는 ++ 미지원
+        if led_index > 2:
+            led_index = 0
 
         # 지정된 위치의 LED만 On 시킨다.
-        for i in range(0, 3):
-            if pos_on_led == i:
-                GPIO.output(LED_PINS[i], LED_ON)
-                if pos_on_led == 0: print('red on')
-                elif pos_on_led == 1: print('green on')
-                elif pos_on_led == 2: print('blue on')
-            else:
-                GPIO.output(LED_PINS[i], LED_OFF)
-
-        '''
-        #----------------------------------------------------------
-        # 다른 방식 #1
-        GPIO.output(PIN_R, LED_ON if pos_on_led == 0 else LED_OFF)
-        GPIO.output(PIN_G, LED_ON if pos_on_led == 1 else LED_OFF)
-        GPIO.output(PIN_B, LED_ON if pos_on_led == 2 else LED_OFF)
-
-        if pos_on_led == 0: print('red on')
-        elif pos_on_led == 1: print('green on')
-        elif pos_on_led == 2: print('blue on')
-
-        # 다른 방식 #2
-        if pos_on_led == 0:
+        if led_index = 0:
+            # RED Led만 On, 나머지는 Off
             GPIO.output(PIN_R, LED_ON)
-            print('red on')
-        else:
-            GPIO.output(PIN_R, LED_OFF)
-
-        if pos_on_led == 1:
-            GPIO.output(PIN_G, LED_ON)
-            print('green on')
-        else:
             GPIO.output(PIN_G, LED_OFF)
-
-        if pos_on_led == 2:
+            GPIO.output(PIN_B, LED_OFF)
+            print('red on')
+        elif led_index = 1:
+            # GREEN Led만 On, 나머지는 Off
+            GPIO.output(PIN_R, LED_OFF)
+            GPIO.output(PIN_G, LED_ON)
+            GPIO.output(PIN_B, LED_OFF)
+            print('green on')
+        elif led_index = 2:
+            # BLUE Led만 On, 나머지는 Off
+            GPIO.output(PIN_R, LED_OFF)
+            GPIO.output(PIN_G, LED_OFF)
             GPIO.output(PIN_B, LED_ON)
             print('blue on')
         else:
+            # 여기에 오면 오류인 셈이지만 예시용으로.
+            GPIO.output(PIN_R, LED_OFF)
+            GPIO.output(PIN_G, LED_OFF)
             GPIO.output(PIN_B, LED_OFF)
-        '''
+            print('all led off')
+
         time.sleep(1.0)   # 1초 대기
 
 except KeyboardInterrupt:       # !중요! CTRL+C 로 종료
